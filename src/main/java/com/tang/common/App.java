992ec38878fd4fe5071148bbbfd4eba30834e19b
xuanku <xuankutang@163.com>
2018-12-13 11:24:30 +0800

    aa

--- src/main/java/com/tang/common/App.java
+++ src/main/java/com/tang/common/App.java
@@ -4,16 +4,14 @@ package com.tang.common;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println(new Integer(100));
-        System.out.println("aaa");
-        System.out.println("bbb");
+public class App {
+	public static void main(String[] args) {
+		System.out.println(new Integer(100));
+		System.out.println("aaa");
 		System.out.println("bbb");
 		System.out.println("bbb");
-        System.out.println("bbb");
+		System.out.println("bbb");
+		System.out.println("bbb");
 		System.out.println(new Integer(100));
 		System.out.println(new Integer(100));
 
