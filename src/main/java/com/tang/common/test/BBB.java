69234b2b26d8823ad751dee65097460c565959cb
xuanku <xuankutang@163.com>
2018-12-21 15:43:19 +0800

    f ef

--- src/main/java/com/tang/common/test/BBB.java
+++ src/main/java/com/tang/common/test/BBB.java
@@ -2,6 +2,9 @@ package com.tang.common.test;
 
 public class BBB {
     public static void main(String[] args) {
+
+        System.out.println(new Integer(200);
+
         System.out.println(new Integer(200));
     }
 }
