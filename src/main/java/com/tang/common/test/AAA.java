ed0a40389643a5d80ca3f893044e2517d746cfac
xuankuku <466099881@qq.com>
2018-12-25 23:29:35 +0800

    Update AAA.java

--- src/main/java/com/tang/common/test/AAA.java
+++ src/main/java/com/tang/common/test/AAA.java
@@ -2,6 +2,6 @@ package com.tang.common.test;
 
 public class AAA {
     public static void main(String[] args) {
-        System.out.println(new Integer(200));
+        System.out.println(new Integer(500));
     }
 }
